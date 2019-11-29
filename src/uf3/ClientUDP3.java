44d7667150669234ee8ddd90c95b4441ebf830be
Marc RG <formu@ASUS-LAPTOP>
2019-11-29 17:38:08 +0100

    1.0

---
+++ src/uf3/ClientUDP3.java
@@ -0,0 +1,62 @@
+package uf3;
+
+import java.net.*;
+import java.io.*;
+
+public class ClientUDP3 {
+	public static void main(String[] args) throws Exception {
+
+		// FLUX PER A ENTRADA ESTÀNDARD
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		// Socket client
+		DatagramSocket clientSocket = new DatagramSocket();
+		byte[] enviats = new byte[1024];
+		byte[] rebuts = new byte[1024];
+
+		// DADES DEL SERVIDOR al qual s'envia el missatge
+		InetAddress IPServidor = InetAddress.getLocalHost();
+		int port = 9800;
+
+		clientSocket.setSoTimeout(5000);
+		DatagramPacket enviament;
+
+		while (!clientSocket.isClosed()) {
+			// INTRODUIR DADES PEL TECLAT
+			System.out.print("Introdueix missatge: ");
+			String cadena = in.readLine();
+			if(cadena.equals("exit")) {
+				clientSocket.close();
+			} else {
+				enviats = cadena.getBytes();
+
+				// ENVIANT DATAGRAMA AL SERVIDOR
+				System.out.println("Enviant " + enviats.length + "bytes al servidor.");
+				enviament = new DatagramPacket(enviats, enviats.length, IPServidor, port);
+				clientSocket.send(enviament);
+
+				// REBENT DATAGRAMA DEL SERVIDOR
+
+				DatagramPacket rebut = new DatagramPacket(rebuts, rebuts.length);
+				System.out.println("Esperant datagrama...");
+				
+				try {
+					clientSocket.receive(rebut);
+				} catch (SocketTimeoutException e) {
+					clientSocket.close();
+					System.out.println("CONNEXIÓ TANCADA");
+				}
+				String majuscula = new String(rebut.getData());
+
+				// ACONSEGUINT INFORMACIÓ DEL DATAGRAMA
+				InetAddress IPOrigen = rebut.getAddress();
+				int portOrigen = rebut.getPort();
+				System.out.println("\tProcedent de: " + IPOrigen + ":" + portOrigen);
+				System.out.println("\tDades: " + majuscula.trim());
+			}
+			
+		}
+		// Tanca el socket
+		clientSocket.close();
+
+	}
+}
